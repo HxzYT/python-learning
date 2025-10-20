782c500d1083ca7c2d43077c2666fa1d6cb2ebcd
HxzYT <blacktiger9010@gmail.com>
2025-10-20 16:42:50 +0800

    turtle example

---
+++ lesson 1.py
@@ -0,0 +1,19 @@
+import turtle
+name = "hamza"
+age = 15
+height = 7.6
+student = True
+
+t1 = turtle.Turtle()
+t1.shape("turtle")
+steps = 100
+
+for i in range(10):
+    t1.forward(steps)
+    t1.left(90)
+
+
+
+
+turtle.done()
+
---
+++ roadmap.py
@@ -0,0 +1,6 @@
+# Topic 1 Data Types ( int flaot string bool)
+# Topic 2 Data Structure ( List Tuple Dictionary Set) 
+# Topic 3 If else
+# Topic 4 Loops ( for loop and while )
+# Topic 5 Function
+# Topic 6 Classes
